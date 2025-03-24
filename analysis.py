7387d2a02a2897291ba580b535bec3df8a4e5405
gabriel-r-s <author@email>
2025-03-24 00:13:04 -0300

    varios

---
+++ analysis.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+np.set_printoptions(precision=3, suppress=True)
+
+serial = pd.read_csv("results/serial.csv", header=None, names=["size", "time"], delimiter=" ")
+threads = pd.read_csv("results/threads.csv", header=None, names=["size", "time"], delimiter=" ")
+gpu = pd.read_csv("results/gpu.csv", header=None, names=["size", "time"], delimiter=" ")
+
+# somente para testes:
+gpu["time"] /= 2.0
+
+FIG_DPI = 300.0
+FIG_SIZE = (10, 4)
+
+
+serial = serial.groupby("size")["time"].mean()
+threads = threads.groupby("size")["time"].mean()
+gpu = gpu.groupby("size")["time"].mean()
+index = serial.index
+
+def plot_time():
+    plt.figure(figsize=FIG_SIZE)
+    plt.plot(index, serial.values, label="Serial", color="red", marker="o")
+    plt.plot(index, threads.values, label="Threads", color="green", linestyle="--", marker="s")
+    plt.plot(index, gpu.values, label="GPU", color="blue", linestyle=":", marker="^")
+    plt.legend()
+    plt.xticks(index, fontsize=8, rotation=45)
+    plt.xlabel("Largura")
+    plt.ylabel("Tempo de Execução")
+
+    plt.savefig("results/time.png", dpi=FIG_DPI)
+    # plt.show()
+
+
+def plot_time_zoomed():
+    plt.figure(figsize=FIG_SIZE)
+    plt.plot(index[:3], serial.values[:3], label="Serial", color="red", marker="o")
+    plt.plot(index[:3], threads.values[:3], label="Threads", color="green", linestyle="--", marker="s")
+    plt.plot(index[:3], gpu.values[:3], label="GPU", color="blue", linestyle=":", marker="^")
+    plt.legend()
+    plt.xticks(index[:3])
+    plt.xlabel("Largura")
+    plt.ylabel("Tempo de Execução")
+
+    plt.savefig("results/time_zoomed.png", dpi=FIG_DPI)
+    # plt.show()
+
+
+def plot_speedup():
+    speedup_serial = np.ones(len(index))
+    speedup_threads = serial.values / threads.values
+    speedup_gpu = serial.values / gpu.values
+
+    plt.figure(figsize=FIG_SIZE)
+    plt.plot(index, speedup_serial, label="Serial", color="red")
+    plt.plot(index, speedup_threads, label="Threads", color="Green", linestyle="--", marker="s")
+    plt.plot(index, speedup_gpu, label="GPU", color="Blue", linestyle=":", marker="^")
+    plt.legend()
+    plt.xticks(index, fontsize=8, rotation=45)
+    plt.xlabel("Largura")
+    plt.ylabel("Speedup")
+    plt.yscale("log")
+
+    plt.savefig("results/speedup.png", dpi=FIG_DPI)
+    # plt.show()
+
+
+def plot_efficiency():
+    speedup_threads = serial.values / threads.values
+    speedup_gpu = serial.values / gpu.values
+
+    efficiency_serial = np.ones(len(index))
+    efficiency_threads = speedup_threads / 8.0
+    efficiency_gpu = speedup_threads / 128.0
+
+    plt.figure(figsize=FIG_SIZE)
+    plt.plot(index, efficiency_serial, label="Serial", color="red")
+    plt.plot(index, efficiency_threads, label="Threads", color="Green", linestyle="--", marker="s")
+    plt.plot(index, efficiency_gpu, label="GPU", color="Blue", linestyle=":", marker="^")
+    plt.legend(loc="upper left")
+    plt.xticks(index, fontsize=8, rotation=45)
+    plt.xlabel("Largura")
+    plt.ylabel("Eficiência")
+    plt.yscale("log")
+
+    plt.savefig("results/efficiency.png", dpi=FIG_DPI)
+    # plt.show()
+
+
+plot_time()
+plot_time_zoomed()
+plot_speedup()
+plot_efficiency()
+
--- run-tests.sh
+++ run-tests.sh
@@ -10,46 +10,54 @@ echo " + $(make gen-input)"
 echo "creating inputs.."
 mkdir -p inputs
 iter=300
-sizes=(10 25 50 100 200 300 500 750 1000)
+sizes=(10 50 100 200 300 400 500 600 700 800 900 1000)
 for size in ${sizes[@]}; do
     filename=inputs/$size.dat
     if [ -f $filename ]; then
         echo " - ${filename} exists."
     else
         echo " + creating ${filename}..."
-        ./gen-input ${iter} ${size} ${size} > $filename
+        ./build/gen-input ${iter} ${size} ${size} > $filename
     fi
 done
 
 repeat=5
 
+mkdir -p results
+
 echo "---------------"
 
 echo "running serial.."
+printf "" > results/serial.csv
 for size in ${sizes[@]}; do
     input=inputs/$size.dat
     ./build/serial < $input >/dev/null 2>&1
     for ((i=0; i<$repeat; i++)); do
-        ./build/serial < $input 2>/dev/null
+        ./build/serial < $input 2>/dev/null | tee -a results/serial.csv
     done
 done
 echo "---------------"
 
 echo "running openmp.."
+printf "" > results/threads.csv
 for size in ${sizes[@]}; do
     input=inputs/$size.dat
     ./build/threads < $input >/dev/null 2>&1
     for ((i=0; i<$repeat; i++)); do
-        ./build/threads < $input 2>/dev/null
+        ./build/threads < $input 2>/dev/null | tee -a results/threads.csv
     done
 done
 echo "---------------"
 
+# remover apos implementar openacc
+cp results/threads.csv results/gpu.csv
+
 # echo "running openacc.."
+# printf "" > results/gpu.csv
 # for size in ${sizes[@]}; do
 #     input=inputs/$size.dat
 #     ./build/gpu < $input >/dev/null 2>&1
 #     for ((i=0; i<$repeat; i++)); do
-#         ./build/gpu < $input 2>/dev/null
+#         ./build/gpu < $input 2>/dev/null | tee -a results/gpu.csv
 #     done
 # done
